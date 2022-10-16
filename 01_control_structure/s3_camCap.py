e0d0098196ee8acdcfc8e84271c619f58c828a45
fudiwara <tfuji@do-johodai.ac.jp>
2022-10-16 11:35:58 +0900

    2

---
+++ 01_control_structure/s3_camCap.py
@@ -0,0 +1,39 @@
+# カメラIDを指定する必要があるため
+# python s3_camCap.py 0
+# のように実行する
+
+import sys
+sys.dont_write_bytecode = True
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(int(sys.argv[1])) # VideoCaptureのインスタンス
+print(cap.isOpened())
+
+# デフォルトのカメラ環境を用いる場合
+# cw, ch = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+cw, ch = 360, 240
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, cw)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, ch)
+print(cw, ch)
+img = np.ones((ch, cw, 3), np.uint8) * 255 # 例外処理用に空の画像を作っておく
+
+while True:
+    ret, frame = cap.read() # VideoCaptureから1フレーム読み込む
+    # print("ret", ret)
+
+    if ret: # 読み込めた場合に処理をする
+        img = frame.copy()
+
+        # 以下に取得した画像 img を用いた処理を記述する
+
+        h, w, _ = frame.shape
+        print(w, h)
+    
+    cv2.imshow("image", img)
+
+    # キー入力を1ms待って、kキーの場合(27 / ESC)だったらBreakする
+    key = cv2.waitKey(1)
+    if key == 27: break
+    elif key == ord("s"): cv2.imwrite("save.jpg", img) # sキーの場合は画像保存する
---
+++ 02_classification/config.py
@@ -0,0 +1,62 @@
+import sys
+sys.dont_write_bytecode = True
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as T
+from torchvision import models
+
+# 画像の一辺のサイズ (この大きさにリサイズされるので要確認)
+cellSize = 200
+
+# クラス全体の数
+classesSize = 3
+
+# 繰り返す回数
+epochSize = 10
+
+# ミニバッチのサイズ
+batchSize = 80
+
+# 学習時のサンプルを学習：検証データに分ける学習側の割合
+splitRateTrain = 0.8
+
+# データ変換
+data_transforms = T.Compose([
+    T.Resize(cellSize),
+    T.RandomRotation(degrees = 15),
+    T.RandomApply([T.GaussianBlur(5, sigma = (0.1, 5.0))], p = 0.5),
+    T.ColorJitter(brightness = 0, contrast = 0, saturation = 0, hue = [-0.2, 0.2]),
+    T.RandomHorizontalFlip(0.5),
+    T.ToTensor()])
+
+def calc_acc(output, label): # 結果が一致するラベルの数をカウントする
+    p_arg = torch.argmax(output, dim = 1)
+    return torch.sum(label == p_arg)
+
+class build_model(nn.Module):
+    def __init__(self):
+        super(build_model, self).__init__()
+        self.model_pret = models.resnet50(pretrained=True)
+        self.bn = nn.BatchNorm1d(1000)
+        self.dropout = nn.Dropout(0.5)
+        self.classifier = nn.Linear(1000, classesSize)
+
+    def forward(self, input):
+        mid_features = self.model_pret(input)
+        x = self.bn(mid_features) # BNを追加
+        x = self.dropout(x) # dropoutを追加
+        output = self.classifier(x)
+        return output
+
+# def build_model():
+#     model = models.resnet50(pretrained = True)
+#     model.fc = nn.Linear(model.fc.in_features, classesSize) # GovalAvgPooling
+#     return model
+
+if __name__ == "__main__":
+    mdl = build_model()
+    print(mdl)
+
+    from torchsummary import summary
+    summary(mdl, (3, cellSize, cellSize))

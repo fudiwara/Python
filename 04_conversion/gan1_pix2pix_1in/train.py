95e943e811305d7909cba47ad4e046827bec50c9
fudiwara <tfuji@do-johodai.ac.jp>
2025-11-12 19:10:53 +0900

    fix

--- 04_conversion/gan1_pix2pix_1in/train.py
+++ 04_conversion/gan1_pix2pix_1in/train.py
@@ -23,8 +23,8 @@ model_G, model_D = cf.Generator(), cf.Discriminator()
 model_G, model_D = nn.DataParallel(model_G), nn.DataParallel(model_D)
 model_G, model_D = model_G.to(DEVICE), model_D.to(DEVICE)
 
-params_G = torch.optim.AdamW(model_G.parameters(), lr=0.0002, betas=(0.5, 0.999), weight_decay=0.01)
-params_D = torch.optim.AdamW(model_D.parameters(), lr=0.000005, betas=(0.5, 0.999), weight_decay=0.01)
+params_G = torch.optim.AdamW(model_G.parameters(), lr=0.00015, betas=(0.5, 0.999), weight_decay=0.01)
+params_D = torch.optim.AdamW(model_D.parameters(), lr=0.000008, betas=(0.5, 0.999), weight_decay=0.01)
 
 # ロスを計算するためのラベル変数 (PatchGAN)
 ones = torch.ones(cf.batchSize, 1, 3, 3).to(DEVICE)
@@ -50,7 +50,7 @@ for i in range(cf.epochSize):
         fake_target_tensor = fake_target.detach() # 偽画像を一時保存
 
         # 偽画像を本物と騙せるようにロスを計算
-        LAMBD = 25.0 # BCEとMAEの係数
+        LAMBD = 10.0 # BCEとMAEの係数
         out = model_D(torch.cat([fake_target, imgs_src], dim=1))
         loss_G_bce = bce_loss(out, ones[:batch_len])
         loss_G_mae = LAMBD * mae_loss(fake_target, real_target)

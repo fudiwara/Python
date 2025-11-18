4779fc55498778ac9771e808e43c0e6be6323302
fudiwara <tfuji@do-johodai.ac.jp>
2025-11-18 13:52:33 +0900

    fix

--- 05_object_detection/detction2MOT/pyt_det/engine.py
+++ 05_object_detection/detction2MOT/pyt_det/engine.py
@@ -29,7 +29,7 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq, sc
     for images, targets in metric_logger.log_every(data_loader, print_freq, header, True):
         images = list(image.to(device) for image in images)
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
-        with torch.cuda.amp.autocast(enabled=scaler is not None):
+        with torch.autocast(device_type=device, enabled=scaler is not None):
             loss_dict = model(images, targets)
             losses = sum(loss for loss in loss_dict.values())
 
